6e383c059ee26d47e5c92a48ccfe2e254687a58c
Miv Anna Donkaewpoo Berg <mibe0002@stud.kea.dk>
2024-02-06 00:08:22 +0100

    backup

--- js/product.js
+++ js/product.js
@@ -1,3 +1,28 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+const url = `https://kea-alt-del.dk/t7/api/products${id}`;
+
+// fange template
+// const template = document.querySelector(".sport_template").content;
+const parent = document.querySelector("section");
+
+function getProduct() {
+  fetch("https://kea-alt-del.dk/t7/api/products/" + id)
+    .then((res) => res.json())
+    .then(showProduct);
+}
+
+function showProduct(product) {
+  const purchaseBox = document.querySelector(".purchase_box");
+
+  document.querySelector(".purchase_box h1").textContent = product.productdisplayname;
+  document.querySelector(".product_2 .brand").textContent = product.brandname;
+  document.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
+  document.querySelector("img").alt = product.productdisplayname;
+}
+
+getProduct();
+
 // Get all elements with the class "press"
 const pressElements = document.querySelectorAll(".press");
 
@@ -13,61 +38,3 @@ pressElements.forEach((press) => {
   });
 });
 // vha. ChatGPT
-
-// press2.addEventListener("click", () => {
-//   //   press2.classList.toggle("active");
-//   dropdown2.classList.toggle("active");
-// });
-
-// const productList = document.querySelector(".product_list");
-
-// let text = {
-//   id: 1163,
-//   gender: "Men",
-//   category: "Apparel",
-//   subcategory: "Topwear",
-//   articletype: "Tshirts",
-//   basecolour: "Blue",
-//   season: "Summer",
-//   productionyear: 2011,
-//   usagetype: "Sports",
-//   productdisplayname: "Sahara Team India Fanwear Round Neck Jersey",
-//   parsed: 1,
-//   soldout: 0,
-//   relid: 1163,
-//   price: 895,
-//   discount: null,
-//   variantname: "Roundneck Jersey",
-//   brandname: "Nike",
-//   brandbio: "Nike, creating experiences for today's athlete",
-//   brandimage: "http://assets.myntassets.com/v1/assets/banners/2015/6/26/1435317851398-23197-3chxv6.jpg",
-//   agegroup: "Adults-Men",
-//   colour1: "NA",
-//   colour2: "NA",
-//   fashiontype: "Fashion",
-//   materialcaredesc: "<p>100% polyester<br>Machine wash</p>",
-//   sizefitdesc: null,
-//   description: "<p>Blue round neck Sahara Team India jersey, has short sleeves, print on the chest and back chest<br><br>Warranty for manufacturing defects: 6 months (not valid on products with more than 20% discount)<br></p>",
-//   styledesc: "<p>Cheer for the Indian cricket team on and off the fields in this blue polo neck jersey from Nike.</p>",
-// };
-
-// function eventListeners() {
-//   window.addEventListener("DoMContentLoaded", () => {
-//     loadJSON();
-//   });
-// }
-
-// function loadJSON() {
-//   fetch("product.json")
-//     .then((response) => response.json())
-//     .then((data) => {
-//       let html = "";
-//       data.forEach((product) => {
-//         html;
-//       });
-//     });
-// }
-
-// const obj = JSON.parse(text);
-
-// console.log("Færdig");
--- js/productlist.js
+++ js/productlist.js
@@ -16,12 +16,14 @@ function showSport(sport) {
   const copy = template.cloneNode(true);
   // ændre indhold
   copy.querySelector("h3").textContent = sport.productdisplayname;
-  // copy.querySelector(".sport_img").src = sport.id;
+
+  copy.querySelector(".sport_img").src = `https://kea-alt-del.dk/t7/images/webp/1000/${sport.id}.webp`;
+  copy.querySelector(".sport_img").alt = sport.productdisplayname;
 
   if (sport.soldout == 0) {
     // produktet er udsolgt
     copy.querySelector(".udsolgt").classList.add("udsolgt");
-    console.log("Soldout value:", sport.soldout);
+    // console.log("Soldout value:", sport.soldout);
     copy.querySelector(".favourit").style.display = "none";
 
     // blur img når udsolgt
@@ -32,6 +34,9 @@ function showSport(sport) {
     copy.querySelector(".udsolgt").style.display = "none";
   }
 
+  copy.querySelector(".read_more").setAttribute("href", `product.html?id=${sport.id}`);
+
+  // copy.querySelector(".read-more").setAttribute("href", `product.html?id=${product.id}`);
   // appende
   document.querySelector(".sport_container").appendChild(copy);
 }
@@ -41,31 +46,6 @@ function blurTil() {
   this.classList.toggle("blur");
 }
 
-//  <article class="sport_article">
-//                         <a href="product.html">
-//                             <div class="sport_img_container">
-//                                 <img class="sport_img">
-//                             </div>
-
-//                             <h3 class="sport_name">
-//                                 Name
-//                             </h3>
-
-//                             <p class="subcat">Tshirts | Nike</p>
-//                             <p class="pris"><span>Prev.</span> 1595,- DKK</p>
-
-//                             <div class="discount">
-//                                 <p class="udsolgt">Udsolgt</p>
-//                                 <p>Now 15600,- DKK</p>
-//                                 <p class="rabat">-34%</p>
-//                             </div>
-
-//                             <p class="sport_tagline">You buy we provide</p>
-//                             <p class="sport_description">Buy this to support as a loyal fan.</p>
-//                         </a>
-//                     </article>
-
-// // {
 //   "id": 1163,
 //   "gender": "Men",
 //   "category": "Apparel",
@@ -79,47 +59,4 @@ function blurTil() {
 //   "discount": null,
 //   "brandname": "Nike",
 //   "soldout": 0
-// }
-
-// window.addEventListener("DOMContentLoaded", init);
-
-// const sportURL = "https://kea-alt-del.dk/t7/api/products";
-
-// let sportTemplate;
-// let sportContainer;
-
-// function init() {
-//   console.log("init");
-
-//   sportTemplate = document.querySelector(".sport_template");
-//   console.log("sportTemplate", sportTemplate);
-
-//   sportContainer = document.querySelector(".sport_container");
-//   console.log("sport_container", sportContainer);
-
-//   fetch(sportURL)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (json) {
-//       showSport(json);
-//     });
-// }
-
-// function showSport(sportJSON) {
-//   // looper og kalder showSport
-//   let sportClone;
-
-//   sportJSON.forEach((sport) => {
-//     console.log("Sport", sport);
-//     sportClone = sportTemplate.cloneNode(true).content;
-//     // sportClone.querySelector(".sport_img").src = sport.sportSmallimage;
-//     // sportClone.querySelector(".sport_img").alt = `Picture of a ${sport.name}`;
-//     sportClone.querySelector(".productdisplayname").textContent = sport.name;
-//     sportClone.querySelector(".productionyear").textContent = sport.tagline;
-//     sportClone.querySelector(".brandname").textContent = sport.description;
-//     sportClone.querySelector(".soldout").textContent = sport.udsolgt;
-//     sportClone.querySelector(".discount").textContent = sport.rabat;
-//     sportContainer.appendChild(sportClone);
-//   });
 // }
