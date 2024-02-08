0a77660ce4224d11ef8d2335b656f888d05f8960
Miv Anna Donkaewpoo Berg <mibe0002@stud.kea.dk>
2024-02-08 10:23:32 +0100

    update

--- js/categories.js
+++ js/categories.js
@@ -1,8 +1,32 @@
+// const urlParams = new URLSearchParams(window.location.search);
+// const category = urlParams.get("category");
+// console.log("category:", category);
+
+// fetch("https://kea-alt-del.dk/t7/api/products?category=" + category)
+//   .then((res) => res.json())
+//   .then(showCategories);
+
+// function showCategories(cats) {
+//   cats.forEach(showCategory);
+// }
+
+// function showCategory(cat) {
+//   // fanger template
+//   const template = document.querySelector("template").content;
+//   // cloner
+//   const clone = template.cloneNode(true);
+//   // ændrer indhold
+//   clone.querySelector("a").textContent = cat.category;
+//   //   console.log(cat.category);
+//   clone.querySelector("a").href = `productlist.html?category=${cat.category}`;
+//   // apppender
+//   document.querySelector(".letterGroup ol").appendChild(clone);
+// }
+
 const urlParams = new URLSearchParams(window.location.search);
 const category = urlParams.get("category");
-console.log("category:", category);
 
-fetch("https://kea-alt-del.dk/t7/api/products?category=" + category)
+fetch("https://kea-alt-del.dk/t7/api/categories")
   .then((res) => res.json())
   .then(showCategories);
 
@@ -11,14 +35,16 @@ function showCategories(cats) {
 }
 
 function showCategory(cat) {
-  // fanger template
+  //fanger vores template
   const template = document.querySelector("template").content;
-  // cloner
+
+  //cloner
   const clone = template.cloneNode(true);
-  // ændrer indhold
+
+  // ændre indhold
   clone.querySelector("a").textContent = cat.category;
-  //   console.log(cat.category);
   clone.querySelector("a").href = `productlist.html?category=${cat.category}`;
-  // apppender
-  document.querySelector(".letterGroup ol").appendChild(clone);
+
+  //appender
+  document.querySelector(".kategoriliste").appendChild(clone);
 }
